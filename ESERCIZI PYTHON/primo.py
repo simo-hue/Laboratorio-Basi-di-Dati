58e018c6dec376099f4bc9ce4d558ca45c18cc30
Mattioli Simone <mattioli.simone.10@gmail.com>
2025-05-13 13:29:17 +0200

    menu di base

--- ESERCIZI PYTHON/primo.py
+++ ESERCIZI PYTHON/primo.py
@@ -47,8 +47,11 @@ def esegui_query(connessione, query):
         if connessione.notices:
             print("Eventuali notifiche: {:s}".format(connessione.notices[-1]))
 
+def rimuovi_entry(connessione, query):
+    print("eliminai")
+
 def main():
-    
+    connessione = None
     try:
         connessione = psycopg2.connect(
         host=DB_HOST,
@@ -72,27 +75,33 @@ def main():
                 print('Esito della creazione della tabella Spese: {:s}'.format(cursore.statusmessage))
                 if connessione.notices:
                     print('Eventuali notifiche: {:s}'.format(connessione.notices[-1]))
-
-                cursore.execute("SELECT COUNT(*) FROM Spese")
-                numero_righe = cursore.fetchone()[0]
-                print("Numero di righe: {:d}".format(numero_righe))
-                
-                if numero_righe == 0:
-                    aggiungi_default(connessione)   
-                
-                # Provo a fare una lettura
-                leggi(connessione)
-                
-                numero_righe = cursore.fetchone()[0]
-                print("Numero di righe: {:d}".format(numero_righe))
-                
-                if numero_righe == 0:
-                    aggiungi_default(connessione)
-                else:
-                    print("La tabella è già presente con delle tuple e quindi nessuna tupla è stata aggiunta.")
-
                 connessione.commit()
-        leggi(connessione)
+
+        while True:
+            print("Menu:")
+            print("1. Aggiungi una nuova")
+            print("2. Leggi tutte le tuple")
+            print("3. Esegui una query specifica")
+            print("4. Eliminare una entry")
+            print("5. Esci")
+            
+            scelta = input("Scegli un'opzione: ")
+            if scelta == "1":
+                aggiungi_default(connessione)
+            elif scelta == "2":
+                leggi(connessione)
+            elif scelta == "3":
+                query = input("Inserisci la query: ")
+                esegui_query(connessione, query)
+            elif scelta == "4":
+                query = input("Inserisci la query: ")
+                rimuovi_entry(connessione, query)
+            elif scelta == "5":
+                break
+            else:
+                print("Scelta non valida. Riprova.")
+
+        
 
     except Exception as e:
         print("Errore:", e)
