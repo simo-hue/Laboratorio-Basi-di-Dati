7d31e56f69d21ab41e0e700ba3ad580120364ce4
Mattioli Simone <mattioli.simone.10@gmail.com>
2025-05-13 13:24:57 +0200

    sistemazione codice

--- ESERCIZI PYTHON/primo.py
+++ ESERCIZI PYTHON/primo.py
@@ -3,51 +3,29 @@ from datetime import date
 from decimal import Decimal
 from mysecrets import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 
-try:
-    connessione = psycopg2.connect(
-    host=DB_HOST,
-    database=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD
-)
+# nuove entry nella tabella di default
+def aggiungi_default(connessione):
+    with connessione.cursor() as cursore:
+        cursore.execute("""
+                            INSERT INTO Spese(data, voce, importo) VALUES
+                            (%s, %s, %s),
+                            (%s, %s, %s),
+                            (%s, %s, %s),
+                            (%s, %s, %s)
+                        """, (
+                            date(2016, 2, 24), "Stipendio", Decimal("0.1"),
+                            date(2016, 2, 24), "Stipendio 'Bis'", Decimal("0.1"),
+                            date(2016, 2, 24), "Stipendio 'Tris'", Decimal("0.1"),
+                            date(2016, 2, 27), "Affitto", Decimal("-0.3")
+                        ))
+    
+    print("Esito dell'inserimento delle 4 tuple: {:s}".format(cursore.statusmessage))
+    
+    if connessione.notices:
+        print("Eventuali notifiche: {:s}".format(connessione.notices[-1]))
 
-    with connessione:
-        with connessione.cursor() as cursore:
-            cursore.execute("""
-                CREATE TABLE IF NOT EXISTS Spese (
-                    id SERIAL PRIMARY KEY,
-                    data DATE NOT NULL,
-                    voce VARCHAR NOT NULL,
-                    importo NUMERIC NOT NULL
-                )
-            """)
-            print('Esito della creazione della tabella Spese: {:s}'.format(cursore.statusmessage))
-            if connessione.notices:
-                print('Eventuali notifiche: {:s}'.format(connessione.notices[-1]))
-
-            cursore.execute("SELECT COUNT(*) FROM Spese")
-            numero_righe = cursore.fetchone()[0]
-
-            if numero_righe == 0:
-                cursore.execute("""
-                    INSERT INTO Spese(data, voce, importo) VALUES
-                    (%s, %s, %s),
-                    (%s, %s, %s),
-                    (%s, %s, %s),
-                    (%s, %s, %s)
-                """, (
-                    date(2016, 2, 24), "Stipendio", Decimal("0.1"),
-                    date(2016, 2, 24), "Stipendio 'Bis'", Decimal("0.1"),
-                    date(2016, 2, 24), "Stipendio 'Tris'", Decimal("0.1"),
-                    date(2016, 2, 27), "Affitto", Decimal("-0.3")
-                ))
-                print("Esito dell'inserimento delle 4 tuple: {:s}".format(cursore.statusmessage))
-            else:
-                print("La tabella è già presente con delle tuple e quindi nessuna tupla è stata aggiunta.")
-
-            connessione.commit()
-
-        with connessione.cursor() as lettore:
+def leggi(connessione):
+    with connessione.cursor() as lettore:
             lettore.execute("SELECT id, data, voce, importo FROM Spese")
             print("Esito della selezione di tutte le tuple: {:s}".format(lettore.statusmessage))
             print('=' * 55)
@@ -62,8 +40,65 @@ try:
             print('-' * 55)
             print("{:>40s} {:10.2f}".format("Totale", tot))
 
-except Exception as e:
-    print("Errore:", e)
-finally:
-    if 'connessione' in locals() and connessione:
-        connessione.close()
+def esegui_query(connessione, query):
+    with connessione.cursor() as cursore:
+        cursore.execute(query)
+        print("Esito della query: {:s}".format(cursore.statusmessage))
+        if connessione.notices:
+            print("Eventuali notifiche: {:s}".format(connessione.notices[-1]))
+
+def main():
+    
+    try:
+        connessione = psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+        )
+        print("Connessione aperta")
+
+        with connessione:
+            with connessione.cursor() as cursore:
+                # creazione della tabella se NON esiste
+                cursore.execute("""
+                    CREATE TABLE IF NOT EXISTS Spese (
+                        id SERIAL PRIMARY KEY,
+                        data DATE NOT NULL,
+                        voce VARCHAR NOT NULL,
+                        importo NUMERIC NOT NULL
+                    )
+                """)
+                print('Esito della creazione della tabella Spese: {:s}'.format(cursore.statusmessage))
+                if connessione.notices:
+                    print('Eventuali notifiche: {:s}'.format(connessione.notices[-1]))
+
+                cursore.execute("SELECT COUNT(*) FROM Spese")
+                numero_righe = cursore.fetchone()[0]
+                print("Numero di righe: {:d}".format(numero_righe))
+                
+                if numero_righe == 0:
+                    aggiungi_default(connessione)   
+                
+                # Provo a fare una lettura
+                leggi(connessione)
+                
+                numero_righe = cursore.fetchone()[0]
+                print("Numero di righe: {:d}".format(numero_righe))
+                
+                if numero_righe == 0:
+                    aggiungi_default(connessione)
+                else:
+                    print("La tabella è già presente con delle tuple e quindi nessuna tupla è stata aggiunta.")
+
+                connessione.commit()
+        leggi(connessione)
+
+    except Exception as e:
+        print("Errore:", e)
+    finally:
+        if 'connessione' in locals() and connessione:
+            connessione.close()
+
+if __name__ == "__main__":
+    main()
