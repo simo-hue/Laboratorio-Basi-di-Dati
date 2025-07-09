d7f05dd06316fdd8e572fb233b0796b84b4d6235
Mattioli Simone <mattioli.simone.10@gmail.com>
2025-07-09 07:23:35 +0200

    es_5 inizio

--- PREPARAZIONE ALL'ESAME ( test )/es_5.sql
+++ PREPARAZIONE ALL'ESAME ( test )/es_5.sql
@@ -1,5 +1,20 @@
 /*
-    
+   CorsoStudi(id, nome, codice, abbreviazione, durataAnni, sede, informativa, id_segreteria)
+    Facolta(id, nome, codice, indirizzo, informativa, id_preside_persona, id_segreteria)
+    Insegn(id, nomeins, codiceins)
+    Discriminante(id, nome, descrizione)
+    InsErogato(id, annoaccademico, id_insegn, id_corsostudi, id_discriminante, modulo, discriminantemodulo,
+    nomemodulo, crediti, programma, id_facolta, hamoduli, id_inserogato_padre, nomeunità, annierogazione)
+    CorsoInFacolta(id, id_corsostudi, id_facolta)
+    Persona(id, cognome, nome, sesso, telefono, email)
+    Docenza(id, id_inserogato, id_persona, id_settore, creditilez, orelez)
+    InsInPeriodo(id, id_inserogato, id_periodolez)
+    PeriodoLez(id, abbreviazione)
+    PeriodoDid(id, annoaccademico, descrizione, discriminante, inizio, fine)
+    Lezione(id, id_periodolez, id_inserogato, giorno, orainizio, datainizio, orafine, datafine)
+    StrutturaServizio(id, nomestruttura, orarioapertura, telefono1, telefono2, fax, email, compiti)
+
+
 */
 SELECT * 
 FROM 
@@ -9,7 +24,23 @@ GROUP BY
 HAVING
 
 /*
-    
+       CorsoStudi(id, nome, codice, abbreviazione, durataAnni, sede, informativa, id_segreteria)
+    Facolta(id, nome, codice, indirizzo, informativa, id_preside_persona, id_segreteria)
+    Insegn(id, nomeins, codiceins)
+    Discriminante(id, nome, descrizione)
+    InsErogato(id, annoaccademico, id_insegn, id_corsostudi, id_discriminante, modulo, discriminantemodulo,
+    nomemodulo, crediti, programma, id_facolta, hamoduli, id_inserogato_padre, nomeunità, annierogazione)
+    CorsoInFacolta(id, id_corsostudi, id_facolta)
+    Persona(id, cognome, nome, sesso, telefono, email)
+    Docenza(id, id_inserogato, id_persona, id_settore, creditilez, orelez)
+    InsInPeriodo(id, id_inserogato, id_periodolez)
+    PeriodoLez(id, abbreviazione)
+    PeriodoDid(id, annoaccademico, descrizione, discriminante, inizio, fine)
+    Lezione(id, id_periodolez, id_inserogato, giorno, orainizio, datainizio, orafine, datafine)
+    StrutturaServizio(id, nomestruttura, orarioapertura, telefono1, telefono2, fax, email, compiti)
+
+
+
 */
 SELECT * 
 FROM 
@@ -19,7 +50,23 @@ GROUP BY
 HAVING
 
 /*
-    
+       CorsoStudi(id, nome, codice, abbreviazione, durataAnni, sede, informativa, id_segreteria)
+    Facolta(id, nome, codice, indirizzo, informativa, id_preside_persona, id_segreteria)
+    Insegn(id, nomeins, codiceins)
+    Discriminante(id, nome, descrizione)
+    InsErogato(id, annoaccademico, id_insegn, id_corsostudi, id_discriminante, modulo, discriminantemodulo,
+    nomemodulo, crediti, programma, id_facolta, hamoduli, id_inserogato_padre, nomeunità, annierogazione)
+    CorsoInFacolta(id, id_corsostudi, id_facolta)
+    Persona(id, cognome, nome, sesso, telefono, email)
+    Docenza(id, id_inserogato, id_persona, id_settore, creditilez, orelez)
+    InsInPeriodo(id, id_inserogato, id_periodolez)
+    PeriodoLez(id, abbreviazione)
+    PeriodoDid(id, annoaccademico, descrizione, discriminante, inizio, fine)
+    Lezione(id, id_periodolez, id_inserogato, giorno, orainizio, datainizio, orafine, datafine)
+    StrutturaServizio(id, nomestruttura, orarioapertura, telefono1, telefono2, fax, email, compiti)
+
+
+
 */
 SELECT * 
 FROM 
@@ -29,7 +76,23 @@ GROUP BY
 HAVING
 
 /*
-    
+       CorsoStudi(id, nome, codice, abbreviazione, durataAnni, sede, informativa, id_segreteria)
+    Facolta(id, nome, codice, indirizzo, informativa, id_preside_persona, id_segreteria)
+    Insegn(id, nomeins, codiceins)
+    Discriminante(id, nome, descrizione)
+    InsErogato(id, annoaccademico, id_insegn, id_corsostudi, id_discriminante, modulo, discriminantemodulo,
+    nomemodulo, crediti, programma, id_facolta, hamoduli, id_inserogato_padre, nomeunità, annierogazione)
+    CorsoInFacolta(id, id_corsostudi, id_facolta)
+    Persona(id, cognome, nome, sesso, telefono, email)
+    Docenza(id, id_inserogato, id_persona, id_settore, creditilez, orelez)
+    InsInPeriodo(id, id_inserogato, id_periodolez)
+    PeriodoLez(id, abbreviazione)
+    PeriodoDid(id, annoaccademico, descrizione, discriminante, inizio, fine)
+    Lezione(id, id_periodolez, id_inserogato, giorno, orainizio, datainizio, orafine, datafine)
+    StrutturaServizio(id, nomestruttura, orarioapertura, telefono1, telefono2, fax, email, compiti)
+
+
+
 */
 SELECT * 
 FROM 
@@ -39,7 +102,23 @@ GROUP BY
 HAVING
 
 /*
-    
+       CorsoStudi(id, nome, codice, abbreviazione, durataAnni, sede, informativa, id_segreteria)
+    Facolta(id, nome, codice, indirizzo, informativa, id_preside_persona, id_segreteria)
+    Insegn(id, nomeins, codiceins)
+    Discriminante(id, nome, descrizione)
+    InsErogato(id, annoaccademico, id_insegn, id_corsostudi, id_discriminante, modulo, discriminantemodulo,
+    nomemodulo, crediti, programma, id_facolta, hamoduli, id_inserogato_padre, nomeunità, annierogazione)
+    CorsoInFacolta(id, id_corsostudi, id_facolta)
+    Persona(id, cognome, nome, sesso, telefono, email)
+    Docenza(id, id_inserogato, id_persona, id_settore, creditilez, orelez)
+    InsInPeriodo(id, id_inserogato, id_periodolez)
+    PeriodoLez(id, abbreviazione)
+    PeriodoDid(id, annoaccademico, descrizione, discriminante, inizio, fine)
+    Lezione(id, id_periodolez, id_inserogato, giorno, orainizio, datainizio, orafine, datafine)
+    StrutturaServizio(id, nomestruttura, orarioapertura, telefono1, telefono2, fax, email, compiti)
+
+
+
 */
 SELECT * 
 FROM 
@@ -49,7 +128,23 @@ GROUP BY
 HAVING
 
 /*
-    
+       CorsoStudi(id, nome, codice, abbreviazione, durataAnni, sede, informativa, id_segreteria)
+    Facolta(id, nome, codice, indirizzo, informativa, id_preside_persona, id_segreteria)
+    Insegn(id, nomeins, codiceins)
+    Discriminante(id, nome, descrizione)
+    InsErogato(id, annoaccademico, id_insegn, id_corsostudi, id_discriminante, modulo, discriminantemodulo,
+    nomemodulo, crediti, programma, id_facolta, hamoduli, id_inserogato_padre, nomeunità, annierogazione)
+    CorsoInFacolta(id, id_corsostudi, id_facolta)
+    Persona(id, cognome, nome, sesso, telefono, email)
+    Docenza(id, id_inserogato, id_persona, id_settore, creditilez, orelez)
+    InsInPeriodo(id, id_inserogato, id_periodolez)
+    PeriodoLez(id, abbreviazione)
+    PeriodoDid(id, annoaccademico, descrizione, discriminante, inizio, fine)
+    Lezione(id, id_periodolez, id_inserogato, giorno, orainizio, datainizio, orafine, datafine)
+    StrutturaServizio(id, nomestruttura, orarioapertura, telefono1, telefono2, fax, email, compiti)
+
+
+
 */
 SELECT * 
 FROM 
@@ -59,7 +154,23 @@ GROUP BY
 HAVING
 
 /*
-    
+       CorsoStudi(id, nome, codice, abbreviazione, durataAnni, sede, informativa, id_segreteria)
+    Facolta(id, nome, codice, indirizzo, informativa, id_preside_persona, id_segreteria)
+    Insegn(id, nomeins, codiceins)
+    Discriminante(id, nome, descrizione)
+    InsErogato(id, annoaccademico, id_insegn, id_corsostudi, id_discriminante, modulo, discriminantemodulo,
+    nomemodulo, crediti, programma, id_facolta, hamoduli, id_inserogato_padre, nomeunità, annierogazione)
+    CorsoInFacolta(id, id_corsostudi, id_facolta)
+    Persona(id, cognome, nome, sesso, telefono, email)
+    Docenza(id, id_inserogato, id_persona, id_settore, creditilez, orelez)
+    InsInPeriodo(id, id_inserogato, id_periodolez)
+    PeriodoLez(id, abbreviazione)
+    PeriodoDid(id, annoaccademico, descrizione, discriminante, inizio, fine)
+    Lezione(id, id_periodolez, id_inserogato, giorno, orainizio, datainizio, orafine, datafine)
+    StrutturaServizio(id, nomestruttura, orarioapertura, telefono1, telefono2, fax, email, compiti)
+
+
+
 */
 SELECT * 
 FROM 
@@ -69,7 +180,23 @@ GROUP BY
 HAVING
 
 /*
-    
+       CorsoStudi(id, nome, codice, abbreviazione, durataAnni, sede, informativa, id_segreteria)
+    Facolta(id, nome, codice, indirizzo, informativa, id_preside_persona, id_segreteria)
+    Insegn(id, nomeins, codiceins)
+    Discriminante(id, nome, descrizione)
+    InsErogato(id, annoaccademico, id_insegn, id_corsostudi, id_discriminante, modulo, discriminantemodulo,
+    nomemodulo, crediti, programma, id_facolta, hamoduli, id_inserogato_padre, nomeunità, annierogazione)
+    CorsoInFacolta(id, id_corsostudi, id_facolta)
+    Persona(id, cognome, nome, sesso, telefono, email)
+    Docenza(id, id_inserogato, id_persona, id_settore, creditilez, orelez)
+    InsInPeriodo(id, id_inserogato, id_periodolez)
+    PeriodoLez(id, abbreviazione)
+    PeriodoDid(id, annoaccademico, descrizione, discriminante, inizio, fine)
+    Lezione(id, id_periodolez, id_inserogato, giorno, orainizio, datainizio, orafine, datafine)
+    StrutturaServizio(id, nomestruttura, orarioapertura, telefono1, telefono2, fax, email, compiti)
+
+
+
 */
 SELECT * 
 FROM 
